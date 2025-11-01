cdd9e60aee6e5c80c8955a1fe556507d4a10cae5
Robotics Computer 1 <monkeybusinessrobotics@gmail.com>
2025-11-01 10:31:16 -0700

    Added a couple no launch autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/TeamCode/AutoBlueNoLaunch.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.TeamCode;
+
+import static java.lang.Math.abs;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+
+@Autonomous(name = "Blue NO LAUNCH", group = "Linear Opmode")
+public class AutoBlueNoLaunch extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        /*
+         * Set up the mecanum driver for analog control
+         */
+        MecanumDriver mecanumDriver = new MecanumDriver(hardwareMap, telemetry, this);
+        mecanumDriver.setMode(1);
+
+        mecanumDriver.moveX(-250, 0.5);
+    }
+}
